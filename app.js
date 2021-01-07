35b439102d923a438aba9b83130bea5fbd9191f7
jorgeemzu <jorge.emzu@gmail.com>
2021-01-07 15:10:17 -0430

    se agrego el carrusel de la seccionde hero con 2 imagenes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const navMenu = document.querySelector('.nav__menu--mobile');
 const hamburger = document.querySelector('.hamburger')
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
 
 
 
@@ -9,5 +12,47 @@ hamburger.addEventListener('click', showMenu);
 
 function showMenu(){
     navMenu.classList.toggle('show__menu')
-    console.log('why')
+
 }
+
+
+slides.forEach((slide, index) =>{
+    slide.style.left = `${index * 100}%`
+} )
+
+let counter = 0;
+
+btnLeft.addEventListener('click', () => {
+    counter--;
+    carrusel();
+
+});
+btnRight.addEventListener('click', () => {
+    counter++;
+    carrusel();
+});
+
+function carrusel(){
+
+    if(counter === slides.length){
+        counter = 0;
+    }
+
+    if(counter < 0){
+        counter = slides.length -1
+    }
+
+
+    slides.forEach(slide => {
+        slide.style.transform = `translateX(-${counter * 100}%)`
+        console.log(counter)
+    })
+}
+
+function slideShow(){
+        carrusel()
+        counter++;
+        console.log('work')
+}
+
+setInterval(slideShow,5000);
